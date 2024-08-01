b38e304d494ddf0358f47edbad51fbccdb64b8f7
nagiyu <otonagiyu@gmail.com>
2024-08-01 20:46:46 +0900

    chore: Add tables for stock exchange and ticker information
    This commit adds two new tables to the database schema: `exchanges` and `tickers`. The `exchanges` table stores information about stock exchanges, including the exchange name, start time, and end time. The `tickers` table stores information about stock tickers, including the ticker name, ticker code, and the ID of the corresponding exchange. Additionally, a new view called `ticker_info` is created by joining the `tickers` and `exchanges` tables to provide combined information about tickers and stock exchanges.
    
    ```

--- containers/postgres/initdb/init.sql
+++ containers/postgres/initdb/init.sql
@@ -10,3 +10,34 @@ CREATE TABLE system_info (
     key VARCHAR(255) NOT NULL,
     value VARCHAR(255) NOT NULL
 );
+
+-- 証券取引所の情報を保存するテーブル
+-- ID、取引所名、取引開始時間、取引終了時間
+CREATE TABLE exchanges (
+    id SERIAL PRIMARY KEY,
+    exchange_name VARCHAR(255) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL
+);
+
+-- 銘柄情報を保存するテーブル
+-- ID、銘柄名、銘柄コード、取引所ID
+CREATE TABLE tickers (
+    id SERIAL PRIMARY KEY,
+    ticker_name VARCHAR(255) NOT NULL,
+    ticker_code VARCHAR(255) NOT NULL,
+    exchange_id INTEGER NOT NULL
+);
+
+-- 銘柄情報、証券取引所情報をJOINしたビュー
+CREATE VIEW ticker_info AS (
+    SELECT
+        tickers.id AS id,
+        tickers.ticker_name AS ticker_name,
+        tickers.ticker_code AS ticker_code,
+        exchanges.exchange_name AS exchange_name
+    FROM
+        tickers
+        JOIN exchanges 
+            ON tickers.exchange_id = exchanges.id
+);
