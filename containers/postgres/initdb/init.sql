4784af2881088ab8b0df3a625e3164f4e14c16bb
nagiyu <otonagiyu@gmail.com>
2024-08-05 16:45:59 +0900

    chore: Add tables for stock exchange and ticker information

--- containers/postgres/initdb/init.sql
+++ containers/postgres/initdb/init.sql
@@ -1,11 +1,14 @@
-CREATE TABLE ticker_urls (
+-- データベース初期化時以外は実行されないので、適用は下記コマンドで
+-- docker exec finance_postgres psql -U postgres -d my_finance_manager_db -f /docker-entrypoint-initdb.d/init.sql
+
+CREATE TABLE IF NOT EXISTS ticker_urls (
     id SERIAL PRIMARY KEY,
     ticker_name VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL
 );
 
 -- キーバリュー形式でシステムで使う情報を保存するテーブル
-CREATE TABLE system_info (
+CREATE TABLE IF NOT EXISTS system_info (
     id SERIAL PRIMARY KEY,
     key VARCHAR(255) NOT NULL,
     value VARCHAR(255) NOT NULL
@@ -13,7 +16,7 @@ CREATE TABLE system_info (
 
 -- 証券取引所の情報を保存するテーブル
 -- ID、取引所名、取引開始時間、取引終了時間
-CREATE TABLE exchanges (
+CREATE TABLE IF NOT EXISTS exchanges (
     id SERIAL PRIMARY KEY,
     exchange_name VARCHAR(255) NOT NULL,
     start_time TIME NOT NULL,
@@ -22,7 +25,7 @@ CREATE TABLE exchanges (
 
 -- 銘柄情報を保存するテーブル
 -- ID、銘柄名、銘柄コード、取引所ID
-CREATE TABLE tickers (
+CREATE TABLE IF NOT EXISTS tickers (
     id SERIAL PRIMARY KEY,
     ticker_name VARCHAR(255) NOT NULL,
     ticker_code VARCHAR(255) NOT NULL,
@@ -30,7 +33,7 @@ CREATE TABLE tickers (
 );
 
 -- 銘柄情報、証券取引所情報をJOINしたビュー
-CREATE VIEW ticker_info AS (
+CREATE OR REPLACE VIEW ticker_info AS (
     SELECT
         tickers.id AS id,
         tickers.ticker_name AS ticker_name,
